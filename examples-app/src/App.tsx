1649589b104cf18fa99f67df29b4eafb646145b7
Anders Westberg <anders.westberg@emellio.com>
2025-02-06 23:34:23 +0100

    Updates test app

--- examples-app/src/App.tsx
+++ examples-app/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
-import { Examples } from '../../src/Examples.jsx'
+import { Examples } from './symlink/Examples.jsx'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>useReactive</h1>
         <Examples />
       </div>
     </>
