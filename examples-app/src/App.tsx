06bf4204441f2302d58a37370e083e8b2c7c7ca0
Anders Westberg <anders.westberg@emellio.com>
2025-02-06 18:11:21 +0100

    Adds test app

---
+++ examples-app/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Examples } from '../../src/Examples.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>useReactive</h1>
+        <Examples />
+      </div>
+    </>
+  )
+}
+
+export default App
